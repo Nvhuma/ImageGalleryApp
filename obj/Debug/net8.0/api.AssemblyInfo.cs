86a02c7f127a958e39f823ef5593abd3dc971e94
Nbvuma <nhlayisekobvuma7@gmail.com>
2024-09-30 12:12:53 +0200

    commits to fix errors

--- obj/Debug/net8.0/api.AssemblyInfo.cs
+++ obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+362c8cee823fb068ef04cd701cf43d23692cf21c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cad812db36521d4c88f12befc99aa32833cc3e57")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
